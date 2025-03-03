bd64b1cb961f6f2f1d595539c4f00bbc4bee75dd
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-03-03 10:25:17 +0700

    Feat: add counter program without styles and themes

--- P3_Teori/app/src/main/java/com/example/compare/MainActivity.java
+++ P3_Teori/app/src/main/java/com/example/compare/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.compare;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+    private TextView tvCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Hary Capri");
+        }
+
+        tvCounter = findViewById(R.id.tvCounter);
+        Button btnPlus = findViewById(R.id.btnPlus);
+        Button btnMinus = findViewById(R.id.btnMinus);
+
+        btnPlus.setOnClickListener(v -> {
+            counter++;
+            tvCounter.setText(String.valueOf(counter));
+        });
+
+        btnMinus.setOnClickListener(v -> {
+            counter--;
+            tvCounter.setText(String.valueOf(counter));
         });
     }
 }
