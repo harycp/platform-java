d1cd6f5c48835da88ac99bf349739acfa6f49e92
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-03-16 12:42:25 +0700

    feat: add Task 5 for theory (Portofolio)

--- Portofolio/app/src/main/java/com/example/portofolio/MainActivity.java
+++ Portofolio/app/src/main/java/com/example/portofolio/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.portofolio;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnJajarGenjangActivity;
-    private Button btnLingkaranActivity;
+
+    private Button btnRedirectActivity;
+    private Button btnProfileActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        btnJajarGenjangActivity = (Button)findViewById(R.id.btn_jajargenjang_activity);
-        btnJajarGenjangActivity.setOnClickListener(this);
+        btnRedirectActivity = (Button)findViewById(R.id.btn_redirect_activity);
+        btnRedirectActivity.setOnClickListener(this);
 
-        btnLingkaranActivity = (Button)findViewById(R.id.btn_lingkaran_activity);
-        btnLingkaranActivity.setOnClickListener(this);
-
-        btnLimasActivity = (Button)findViewById(R.id.btn_limas_activity);
-        btnLimasActivity.setOnClickListener(this);
+        btnProfileActivity = (Button)findViewById(R.id.btn_profile_activity);
+        btnProfileActivity.setOnClickListener(this);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -40,19 +39,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_jajargenjang_activity) {
-            Intent jajarGenjangIntent = new Intent(MainActivity.this, JajarGenjangActivity.class);
-            startActivity(jajarGenjangIntent);
+        if (v.getId() == R.id.btn_redirect_activity) {
+            Intent redirectIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.haryc.me"));
+            startActivity(redirectIntent);
         }
 
-        if (v.getId() == R.id.btn_lingkaran_activity) {
-            Intent lingkaranIntent = new Intent(MainActivity.this, LingkaranActivity.class);
-            startActivity(lingkaranIntent);
-        }
-
-        if (v.getId() == R.id.btn_limas_activity) {
-            Intent limasIntent = new Intent(MainActivity.this, LimasActivity.class);
-            startActivity(limasIntent);
+        if (v.getId() == R.id.btn_profile_activity) {
+            Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(profileIntent);
         }
     }
 }
