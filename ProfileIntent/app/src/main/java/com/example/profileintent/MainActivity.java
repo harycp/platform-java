2d532932481ab7ebc27e7425e61fbfbc7b45f2bb
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-03-23 13:22:26 +0700

    feat: add Profile Intent

--- ProfileIntent/app/src/main/java/com/example/profileintent/MainActivity.java
+++ ProfileIntent/app/src/main/java/com/example/profileintent/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.profileintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnProfileActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnProfileActivity = (Button)findViewById(R.id.btn_profile_activity);
+        btnProfileActivity.setOnClickListener(this);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_profile_activity) {
+            Profile mProfile = new Profile();
+            mProfile.setFullName("Hary Capri");
+            Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
+            profileIntent.putExtra(ProfileActivity.EXTRA_PROFILE, mProfile);
+            startActivity(profileIntent);
+        }
     }
 }
