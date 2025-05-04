7294521e389b5ec2ff338c18de652fa16395256c
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-05-04 16:47:08 +0700

    feat: add images and booksdata

---
+++ MyPersonalLibrary/app/src/main/java/com/example/mypersonallibrary/Book.java
@@ -0,0 +1,31 @@
+package com.example.mypersonallibrary;
+
+public class Book {
+    private String title;
+    private String detail;
+    private int photo;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
---
+++ MyPersonalLibrary/app/src/main/java/com/example/mypersonallibrary/BooksData.java
@@ -0,0 +1,15 @@
+package com.example.mypersonallibrary;
+
+public class BooksData {
+    private static String[] bookTitles = {
+
+    };
+
+    private static String[] bookDetails = {
+
+    };
+
+    private static int[] bookImages = {
+
+    };
+}
